7f6f74492504ebae81f5fa44758070b4aa97a30f
Senku <senku6593@gmail.com>
2025-12-19 15:03:18 +0300

    Change BOT_TOKEN in config.py
    Updated BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8572750987:AAGHL1WKnWOfjchc-szBSwAOuTsJvNCiSlM"
+BOT_TOKEN = "8222916053:AAHR7OijtVpXF3UNopgCBoscGxlbFSrn69Y"
 
 ADMINS = [8414792453, 1553715060]
 
@@ -12,3 +12,4 @@ CHANNEL_IDS = [
     -1001959619008,   # Wiazy (канал)
     -1003154022327    # Wiazy Chat (группа)
 ]
+
